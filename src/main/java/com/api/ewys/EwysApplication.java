400e6520ff9ca64f57e658fd0584d20903d6c80d
Max Ivanov <mamalenaralalend@gmail.com>
2023-02-06 20:58:37 +0300

    init commit

---
+++ src/main/java/com/api/ewys/EwysApplication.java
@@ -0,0 +1,13 @@
+package com.api.ewys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ewys/EwysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ewys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
